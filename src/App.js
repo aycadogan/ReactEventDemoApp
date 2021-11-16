bf967fc13ad854f7030e090a14009fe72acd0921
aycadogan89 <doganayca89@gmail.com>
2021-11-15 19:31:07 -0800

    useFetch

--- src/App.js
+++ src/App.js
@@ -1,62 +1,45 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useCallback} from 'react'
+import { useFetch } from './hooks/useFetch'
 
 import './App.css';
 
-import Modal from './components/Modal'
 import EventList from './components/EventList';
-import NewEventForm from './components/NewEventForm';
 
 function App() {
-  const [showModal, setShowModal] = useState(false)
-  const [showEvents, setShowEvents] = useState(true)
-  const [events, setEvents] = useState([])
 
-  console.log(events)
+  // const [events, setEvents] = useState([])
+  const [url, setUrl] = useState('http://localhost:3000/events')
+  const { data: events } = useFetch(url)
   
+  // const fetchEvent = useCallback(async () => {
+  //   const response = await fetch(url)
+  //   const events = await response.json()
+  //   setEvents(events)
+  // }, [url])
+
   useEffect(() => {
-    fetch('http://localhost:3000/events')
-      .then(response => response.json())
-      .then(json => setEvents(json))
-      .catch(err => console.log(err))
-  }, [])
-  
-  const handleClose = () => {
-    setShowModal(false)
-  }
-  
-  const handleClick = (id) => {
-    setEvents((prevEvents) => {
-      return prevEvents.filter(event => id !== event.id)
-    })
-  }
-  
+    // fetch(url)
+    //   .then(response => response.json())
+    //   .then(json => setEvents(json))
+    //   .catch(err => console.log(err))
+    
+    // const fetchEvent = async () => {
+    //   const response = await fetch(url)
+    //   const events = await response.json()
+    //   setEvents(events)
+    // }
+    // fetchEvent()
+  }, [url])
 
   return (
     <div className="App">
-      <h1 style={{ color: "red", paddingTop: "50px",}}>Weekend Events</h1>
+      <h1 style={{ color: "red", paddingTop: "50px",}}>Weekend Events</h1>  
+      {events && <EventList events={events} />}
 
-      {showEvents && (
-        <div>
-          <button onClick={() => setShowEvents(false)}>Hide Events</button>
-        </div>
-      )}
-      {!showEvents && (
-        <div>
-          <button onClick={() => setShowEvents(true)}>Show Events</button>
-        </div>
-      )}
-
-      {showEvents && <EventList events={events} handleClick={handleClick} />}
-
-      {showModal && (
-        <Modal handleClose={handleClose}>
-          <NewEventForm />
-        </Modal>
-      )}
-
-
-      <div>
-        <button onClick={() => setShowModal(true)}>Show Modal</button>
+      <div className="filters">
+        <button onClick={() => setUrl('http://localhost:3000/events?loc=Europe')}>European Events </button>
+        {" "}
+        <button onClick={() => setUrl('http://localhost:3000/events')}>All Events</button>
       </div>
     </div>
   );
